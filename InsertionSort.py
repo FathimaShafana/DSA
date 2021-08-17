391391f7380eef781c0de39c2af9c935295e8780
FathimaShafana <77454499+FathimaShafana@users.noreply.github.com>
2021-08-17 14:20:44 +0530

    Update InsertionSort

--- InsertionSort.py
+++ InsertionSort.py
@@ -1,2 +1,36 @@
+def insertionSortAsc(array):
+ 
+    
+    for i in range(1, len(array)):
+ 
+        key = array[i]
+ 
+        j = i-1
+        while j >= 0 and key < array[j] :
+                array[j + 1] = array[j]
+                j = j-1
+        array[j + 1] = key
+ 
+def insertionSortDesc(array):
+ 
+    
+    for i in range(1, len(array)):
+ 
+        key = array[i]
+ 
+        j = i-1
+        while j >= 0 and key > array[j] :
+                array[j + 1] = array[j]
+                j = j- 1
+        array[j + 1] = key
+
+array = [25, 31, 56, 11, 10, 5]
+insertionSortAsc(array)
+print("In Ascending Order")
+for i in range(len(array)):
+    print (array[i])
+ 
+print("\nIn Descending Order")
+insertionSortDesc(array)
 for i in range(len(array)):
     print (array[i])
